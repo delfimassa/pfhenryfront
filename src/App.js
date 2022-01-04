8227962ca727fce515c4cde2b0702c1cb52b7d62
Delfi <delfimassa@gmail.com>
2022-01-04 13:12:12 -0300

    componentes ppales y un par de rutas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Landing from "./Components/Home/Landing";
+import Navbar from "./Components/Common/Navbar";
+import Footer from "./Components/Common/Footer";
+import Login from "./Components/Login"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        
+      </Routes>
+      <Footer></Footer>
+    </BrowserRouter>
   );
 }
 

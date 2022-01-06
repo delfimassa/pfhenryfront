050dd3a6a4f5077499c9adb4a58725a5c6793dda
Kevin Torres <kevin_akd@hotmail.com>
2022-01-06 14:06:30 -0500

    add: login and register

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import Landing from "./Components/Home/Landing";
-import Navbar from "./Components/Common/Navbar";
-import Footer from "./Components/Common/Footer";
-import Login from "./Components/Login"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// import Landing from "./Components/Home/Landing";
+// import Navbar from "./Components/Common/Navbar";
+// import Footer from "./Components/Common/Footer";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar></Navbar>
+      {/* <Navbar></Navbar> */}
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        
+        {/* <Route path="/" element={<Landing />} /> */}
+        <Route path="/" element={<Login />} /> {/* AÑADIR PATH LOGIN*/}
+        <Route path="/register" element={<Register />} />
       </Routes>
-      <Footer></Footer>
+      {/* <Footer></Footer> */}
     </BrowserRouter>
   );
 }

68d6809242287c838dd159b36f39712632a16545
Riesedust <lucianoleon2003@gmail.com>
2022-01-07 18:29:23 -0300

    add cards

---
+++ src/Components/Cards/Cards.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Card from './Card'
+
+function Cards() {
+    return (
+        <Link to={`id o nombre?`}> 
+            <Card 
+            
+            //valores de las peluquerias
+            
+            />
+        </Link>
+    )
+}
+
+export default Cards

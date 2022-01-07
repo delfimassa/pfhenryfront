d35593259faaa73d8b3ec171dde4ae5ed010dfdd
Riesedust <lucianoleon2003@gmail.com>
2022-01-07 18:34:39 -0300

    add route home

--- src/App.js
+++ src/App.js
@@ -8,21 +8,22 @@ import Footer from "./Components/Footer";
 import SearchBar from './Components/Home/SearchBar';
 // import Landing from "./Components/Home/Landing";
 import Navbar from "./Components/Common/Navbar";
-import Footer from "./Components/Common/Footer";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
+import Home from "./Components/Visual/Home";
+import "bootswatch/dist/lux/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-            <Navbar />
+      {/* <Home /> */}
       <Routes>
         {/* <Route path="/" element={<Landing />} /> */}
         <Route path="/" element={<Login />} /> {/* AÑADIR PATH LOGIN*/}
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />}/>
       </Routes>
       <Footer />
-      <SearchBar />
     </div>
   );
 }

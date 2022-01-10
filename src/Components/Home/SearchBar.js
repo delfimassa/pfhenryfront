e8c231e6338dba0392ba8ba2c66cbe9220d23634
Your Name <you@example.com>
2022-01-10 05:02:23 -0300

    se agregaron estilos y se mejoraron los detalles

--- src/Components/Home/SearchBar.js
+++ src/Components/Home/SearchBar.js
@@ -1,5 +1,6 @@
 import { React, useState } from "react";
 import { useDispatch } from "react-redux";
+import "./SearchBar.css";
 
 const SearchBar = () => {
   const [search, setSearch] = useState("");
@@ -15,33 +16,46 @@ const SearchBar = () => {
   }
 
   return (
+    // <div className="barraSearch">
+    //   <div className="conenidoTitleBarra">
+    //     <h2>Encontrá tu peluquería donde sea que estes!</h2>
+    //     <form className="d-flex" onSubmit={(e) => handleOnSubmit(e)}>
+    //       <input
+    //         className="form-control input-search"
+    //         type="text"
+    //         placeholder="Search"
+    //         onChange={(e) => handleOnChange(e)}
+    //       />
+    //       <select
+    //         style={{ outline: "none", width: "200px" }}
+    //         className="me-sm-2"
+    //       >
+    //         <option name="ciudad">Ciudad</option>
+    //         <option name="servicio">Servicio</option>
+    //       </select>
+    //       <button
+    //         style={{ borderRadius: "3px" }}
+    //         type="submit"
+    //         className="btn btn-primary my-2 my-sm-0"
+    //       >
+    //         Search
+    //       </button>
+    //     </form>
+    //   </div>
+    // </div>
     <div className="barraSearch">
-      <div className="conenidoTitleBarra">
-        <h2>Encontrá tu peluquería donde sea que estes!</h2>
-        <form className="d-flex" onSubmit={(e) => handleOnSubmit(e)}>
-          <input
-            className="form-control input-search"
-            type="text"
-            placeholder="Search"
-            onChange={(e) => handleOnChange(e)}
-          />
-          <select
-            style={{ outline: "none", width: "200px" }}
-            className="me-sm-2"
-          >
-            <option name="ciudad">Ciudad</option>
-            <option name="servicio">Servicio</option>
-          </select>
-          <button
-            style={{ borderRadius: "3px" }}
-            type="submit"
-            className="btn btn-primary my-2 my-sm-0"
-          >
-            Search
-          </button>
-        </form>
-      </div>
-    </div>
+            <div className='conenidoTitleBarra'>
+                <h2>Encontrá tu peluquería donde sea que estes!</h2>
+                <form className="d-flex" onSubmit={(e) => handleOnSubmit(e)}>
+                    <input className="form-control input-search" type="text" placeholder="Search" onChange={(e) => handleOnChange(e)} />
+                    <select style={{ outline: 'none', width: '200px' }} className="me-sm-2">
+                        <option name="ciudad">Ciudad</option>
+                        <option name="servicio">Servicio</option>
+                    </select>
+                    <button style={{borderRadius:"3px"}} type="submit" className="btn btn-primary my-2 my-sm-0">Search</button>
+                </form>
+                </div>
+                </div>
   );
 };
 

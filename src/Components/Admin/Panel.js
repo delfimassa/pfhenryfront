4574dc1d7659c3805668b027eabb578ac008815f
Delfi <delfimassa@gmail.com>
2022-01-15 17:38:45 -0300

    faltaban mas conflictos se chiflo visual

--- src/Components/Admin/Panel.js
+++ src/Components/Admin/Panel.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import axios from 'axios'
-import React, {useState} from 'react'
-import { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-
-function Panel() {
-
-    const [pelu, setPelu] = useState(null);
-  let { id } = useParams();
-
-  useEffect(() => {
-    // dispatch(searchPokemonId(id))
-=======
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import axios from "axios";
@@ -21,33 +7,11 @@ function Panel() {
   let { id } = useParams();
 
   useEffect(() => {
->>>>>>> db603768a6424816438982a91aa2e433be89acb1
     axios.get(`http://localhost:4000/peluqueria/${id}`).then((response) => {
       setPelu([response.data]);
     });
   }, []);
 
-<<<<<<< HEAD
-  console.log("pelu", pelu);
-
-  return (
-    <div>
-      {/* <p>{selectedPelu.name}</p> */}
-      {pelu == null ? <div>Loading</div> :
-      <div>
-          <p>{pelu[0].name}</p>
-          <p>{pelu[0].address}</p>
-          <p>{pelu[0].phone}</p>
-          <img src={pelu[0].avatar}/> 
-      </div>
-      }
-    </div>
-  );
-
-}
-
-export default Panel
-=======
   console.log(id);
   console.log(pelu[0]);
   return (
@@ -70,4 +34,3 @@ export default Panel
 }
 
 export default Panel;
->>>>>>> db603768a6424816438982a91aa2e433be89acb1

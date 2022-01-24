54a603130572169bfeff92e10471c4f67abe9f3a
matias-rao <61117058+matias-rao@users.noreply.github.com>
2022-01-24 03:22:09 -0300

    integracion mp

--- src/Components/Carrito.js
+++ src/Components/Carrito.js
@@ -2,10 +2,31 @@ import React from 'react';
 
 const Carrito = () => {
     return (
-        <div>
-            <p>carritoo</p>
+        <div className='container'>
+            <h2>prueba mp</h2>
+            <div className='row'>
+                <div className='col-md-6'>
+                    <div className='card'>
+                        <div className='card-header'> 
+                            <h3>turno pelu</h3>
+                        </div>
+                        <div className='card-body'>
+                            <p>Corte y barba</p>
+                            <p>Precio $800</p>
+                            <div>
+                                <form action='http://localhost:4000/MP/create' method='POST'>
+                                    <input type='hidden' value='1' name='price'/>
+                                    <input type='hidden' value='Corte y barba' name='title'/>
+                                    <input type='submit' value='comprar' className='btn btn-primary'/>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
+
 
 export default Carrito;

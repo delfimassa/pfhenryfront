72e618af7fe2b72b9cd1c577cdbd55ba476166b9
Delfi <delfimassa@gmail.com>
2022-01-24 04:31:29 -0300

    img x defecto

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -15,12 +15,7 @@ function Card({ name, address, city, state, rating, avatar, schedule, phone }) {
     <div className="parent-card row mb-3 p-0">
       <div className="containerImg m-0 p-0 col-md-4 col-sm-12">
         {
-          <img className="imagenes w-100" src={avatar} alt="Avatar" /> ||
-          <img
-            className="imagenes w-100"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQakURH-9kvEQgPlj0sZfnAmMhnS6V9t_Tglw&usqp=CAU"
-            alt="Avatar"
-          />
+          <img className="imagenes w-100" src={avatar? avatar :  "https://images.pexels.com/photos/3993125/pexels-photo-3993125.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"} alt="Avatar" /> 
         }
       </div>
 
